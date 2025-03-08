bbc74b8cd38170480c237e5cafdc6b950b872cfd
pickleprat <prat16here@gmail.com>
2025-03-08 20:30:29 +0530

    Added files

--- frontend.py
+++ frontend.py
@@ -1,8 +1,18 @@
 import streamlit as st
 from prompts import meta_prompt
 import base64
+import openai 
+import dotenv 
+import os 
+
+dotenv.load_dotenv()
 
 st.set_page_config(layout="wide") 
+
+model: str = "gpt-4o-mini"
+OPENAI_API_KEY : str = os.getenv("OPENAI_API_KEY") 
+
+client = openai.OpenAI(api_key=OPENAI_API_KEY)
 
 def display_pdf(file):
     base64_pdf = base64.b64encode(file.read()).decode("utf-8")
@@ -25,11 +35,23 @@ def rag_page():
     with col1:
         normal_prompt = st.text_area("User Prompt", height=400, placeholder="Enter your prompt here...")
     with col2:
-        engineered_prompt = st.text_area("Engineered Prompt", height=400, placeholder="Engineered prompt will appear here...")
+        engineered_prompt = st.text_area("Engineered Prompt", 
+                                         height=400, 
+                                         disabled=True, 
+                                         placeholder=st.session_state.engineered_prompt)
     
     if normal_prompt:
-        engineered_prompt = normal_prompt.strip().upper()  
-        st.session_state.engineered_prompt = engineered_prompt
+        engineered_prompt = meta_prompt.format(normal_prompt)  
+        response = client.chat.completions.create(
+            model=model, 
+            messages=[{
+                "role": "user", 
+                "content": engineered_prompt, 
+            }], 
+        ) 
+
+        # adding code to modify prompt here
+        st.session_state.engineered_prompt = response.choices[0].message.content 
 
     st.subheader("Outputs")
     out_col1, out_col2 = st.columns(2)
@@ -41,10 +63,6 @@ def rag_page():
             st.write("No user prompt provided.")
     with out_col2:
         st.markdown("**Output for Engineered Prompt:**")
-        if 'engineered_prompt' in st.session_state or engineered_prompt:
-            st.write(f"Processed output for: {engineered_prompt}")
-        else:
-            st.write("No engineered prompt provided.")
 
 def main():
     st.sidebar.title("Navigation")
